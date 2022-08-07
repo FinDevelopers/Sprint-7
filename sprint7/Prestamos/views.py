458226013e60d00e4500a1ea65acc3e140f141af
Sophia Alvarez <s.alvarez.bulfon@gmail.com>
2022-08-07 17:01:36 -0300

    Formulario

--- sprint7/Prestamos/views.py
+++ sprint7/Prestamos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import PrestamoForm
 
 # Create your views here.
+def formularios(request):
+    usuario_nombre = "Armando Esteban Quito"
+    prestamo_form = PrestamoForm
+    print(prestamo_form.declared_fields)
+    return render(request, 'Prestamos/formularios.html', {'usuario_nombre': usuario_nombre, 'prestamo_form': prestamo_form})
