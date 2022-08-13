5a6858c626e6a57329b50ca02c43c450fb153f91
SantiagoPivato <santiago.pivato@pbs-arg.com>
2022-08-13 01:54:39 -0300

    admin models clientes + verbose_names models clientes

--- sprint7/Cuentas/admin.py
+++ sprint7/Cuentas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TipoCuenta
 
 # Register your models here.
+class TipoCuentaAdmin(admin.ModelAdmin):
+    list_display = ['act_id', 'act_name']
+    list_display_links = ['act_name']
+admin.site.register(TipoCuenta, TipoCuentaAdmin)
+
+
--- sprint7/Cuentas/models.py
+++ sprint7/Cuentas/models.py
@@ -1,3 +1,29 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class TipoCuenta(models.Model):
+    act_id = models.AutoField(primary_key=True, verbose_name='ID')
+    act_name = models.TextField(blank=True, null=True, verbose_name='nombre')
+
+    def __str__(self):
+        return self.act_name
+
+    class Meta:
+        managed = False
+        db_table = 'tipo_cuenta'
+        verbose_name = 'Tipo de Cuenta'
+        verbose_name_plural = 'Tipos de Cuenta'
+        ordering = ['act_id']
+
+
+class Cuenta(models.Model):
+    account_id = models.AutoField(primary_key=True, verbose_name='ID')
+    customer_id = models.IntegerField()
+    account_type = models.ForeignKey('TipoCuenta', models.DO_NOTHING, db_column='account_type')
+    balance = models.IntegerField()
+    iban = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'cuenta'
